540d87453f8d5d06126aeb99cc309a047b0d77b0
Gerardo Lopez Reyes <glopezr@scotiabank.com.mx>
2022-03-30 10:32:25 -0600

    add missing bean app config

--- application/src/main/java/com/gbm/brokeragefirmapi/config/AppPrimaryAdapterConfiguration.java
+++ application/src/main/java/com/gbm/brokeragefirmapi/config/AppPrimaryAdapterConfiguration.java
@@ -1,8 +1,14 @@
 package com.gbm.brokeragefirmapi.config;
 
 import com.gbm.brokeragefirmapi.domain.service.CreateInvestmentAccountService;
+import com.gbm.brokeragefirmapi.domain.service.SendBuyOrderService;
+import com.gbm.brokeragefirmapi.domain.service.SendOrderService;
 import com.gbm.brokeragefirmapi.port.primary.CreateInvestmentAccountServicePort;
+import com.gbm.brokeragefirmapi.port.primary.SendOrderServicePort;
 import com.gbm.brokeragefirmapi.port.secondary.AccountRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.IssuerRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.OrderRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.StockRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +18,23 @@ import org.springframework.context.annotation.Configuration;
 public class AppPrimaryAdapterConfiguration {
 
     private final AccountRepositoryPort accountRepositoryPort;
+    private final StockRepositoryPort stockRepositoryPort;
+    private final OrderRepositoryPort orderRepositoryPort;
+    private final IssuerRepositoryPort issuerRepositoryPort;
 
     @Bean
     public CreateInvestmentAccountServicePort createInvestmentAccountServicePort() {
 
         return new CreateInvestmentAccountService(this.accountRepositoryPort);
+    }
+
+    @Bean
+    public SendOrderServicePort sendOrderServicePort() {
+
+        return new SendOrderService(
+                this.accountRepositoryPort,
+                this.stockRepositoryPort,
+                new SendBuyOrderService(this.orderRepositoryPort, this.issuerRepositoryPort, this.accountRepositoryPort)
+        );
     }
 }
--- application/src/main/java/com/gbm/brokeragefirmapi/config/AppSecondaryAdapterConfiguration.java
+++ application/src/main/java/com/gbm/brokeragefirmapi/config/AppSecondaryAdapterConfiguration.java
@@ -1,8 +1,10 @@
 package com.gbm.brokeragefirmapi.config;
 
-import com.gbm.brokeragefirmapi.AccountJpaRepository;
-import com.gbm.brokeragefirmapi.AccountJpaRepositoryAdapter;
+import com.gbm.brokeragefirmapi.*;
 import com.gbm.brokeragefirmapi.port.secondary.AccountRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.IssuerRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.OrderRepositoryPort;
+import com.gbm.brokeragefirmapi.port.secondary.StockRepositoryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,31 @@ import org.springframework.context.annotation.Configuration;
 public class AppSecondaryAdapterConfiguration {
 
     private final AccountJpaRepository accountJpaRepository;
+    private final IssuerJpaRepository issuerJpaRepository;
+    private final OrderJpaRepository orderJpaRepository;
+    private final StockJpaRepository stockJpaRepository;
 
     @Bean
     public AccountRepositoryPort accountRepositoryPort() {
 
         return new AccountJpaRepositoryAdapter(this.accountJpaRepository);
+    }
+
+    @Bean
+    public IssuerRepositoryPort issuerRepositoryPort() {
+
+        return new IssuerJpaRepositoryAdapter(this.issuerJpaRepository);
+    }
+
+    @Bean
+    public OrderRepositoryPort orderRepositoryPort() {
+
+        return new OrderJpaRepositoryAdapter(this.orderJpaRepository);
+    }
+
+    @Bean
+    public StockRepositoryPort stockRepositoryPort() {
+
+        return new StockJpaRepositoryAdapter(this.stockJpaRepository);
     }
 }
