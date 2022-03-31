40ea622fb1bdf3c0b06393aba4308c70552dfa67
Gerardo Lopez Reyes <glopezr@scotiabank.com.mx>
2022-03-31 15:26:39 -0600

    add account rest controller test

---
+++ infrastructure/spring-web-rest-adapter/src/test/java/com/gbm/brokeragefirmapi/AccountRestControllerAdapterTest.java
@@ -0,0 +1,91 @@
+package com.gbm.brokeragefirmapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gbm.brokeragefirmapi.domain.model.Account;
+import com.gbm.brokeragefirmapi.domain.model.Order;
+import com.gbm.brokeragefirmapi.port.primary.CreateInvestmentAccountUseCase;
+import com.gbm.brokeragefirmapi.port.primary.SendOrderUseCase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.gbm.brokeragefirmapi.factory.AccountRestControllerMockFactory.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AccountRestControllerAdapter.class)
+class AccountRestControllerAdapterTest {
+
+    private static final String CREATE_ACCOUNT_ENDPOINT = "/api/v1/accounts";
+    private static final String SEND_ORDER_ENDPOINT = "/api/v1/accounts/1/orders";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CreateInvestmentAccountUseCase createInvestmentAccountUseCase;
+
+    @MockBean
+    private SendOrderUseCase sendOrderUseCase;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createInvestmentAccount_shouldReturnHttpStatusCreated_whenAccountIsCreated() throws Exception {
+
+        when(this.createInvestmentAccountUseCase.createInvestmentAccount(any(Account.class)))
+                .thenReturn(createMockAccount());
+
+        final var mockCreateAccountRequest = createMockCreateAccountRequest();
+
+        final var content = this.objectMapper.writeValueAsString(mockCreateAccountRequest);
+
+        final var requestBuilder = post(CREATE_ACCOUNT_ENDPOINT)
+                .content(content)
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.id").value(ACCOUNT_ID))
+                .andExpect(jsonPath("$.cash").value(CASH))
+                .andExpect(jsonPath("$.issuers").isArray())
+                .andExpect(jsonPath("$.issuers").isEmpty());
+    }
+
+    @Test
+    void sendOrder() throws Exception {
+
+        when(this.sendOrderUseCase.sendOrder(any(Order.class)))
+                .thenReturn(createMockProcessedOrder());
+
+        final var mockSendOrderRequest = createMockSendOrderRequest();
+
+        final var content = this.objectMapper.writeValueAsString(mockSendOrderRequest);
+
+        final var requestBuilder = post(SEND_ORDER_ENDPOINT)
+                .content(content)
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON);
+
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.current_balance.cash").value(CASH))
+                .andExpect(jsonPath("$.current_balance.issuers").isArray())
+                .andExpect(jsonPath("$.current_balance.issuers").isNotEmpty())
+                .andExpect(jsonPath("$.business_errors").isArray())
+                .andExpect(jsonPath("$.business_errors").isEmpty());
+    }
+}
---
+++ infrastructure/spring-web-rest-adapter/src/test/java/com/gbm/brokeragefirmapi/factory/AccountRestControllerMockFactory.java
@@ -0,0 +1,82 @@
+package com.gbm.brokeragefirmapi.factory;
+
+import com.gbm.brokeragefirmapi.CreateAccountRequest;
+import com.gbm.brokeragefirmapi.SendOrderRequest;
+import com.gbm.brokeragefirmapi.domain.model.Account;
+import com.gbm.brokeragefirmapi.domain.model.Issuer;
+import com.gbm.brokeragefirmapi.domain.model.ProcessedOrder;
+import com.gbm.brokeragefirmapi.domain.model.ProcessedOrder.CurrentBalance;
+import com.gbm.brokeragefirmapi.domain.model.Stock;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.gbm.brokeragefirmapi.domain.model.Order.OrderOperation.BUY;
+import static java.time.LocalTime.NOON;
+import static java.time.ZoneId.systemDefault;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+public class AccountRestControllerMockFactory {
+
+    public static final BigDecimal CASH = BigDecimal.valueOf(1_000);
+    public static final Long ACCOUNT_ID = 1L;
+    public static final String ISSUER_NAME = "AAPL";
+    public static final BigDecimal SHARE_PRICE = BigDecimal.valueOf(80L);
+    public static final Integer TOTAL_SHARES = 2;
+    public static final Integer ISSUER_ID = 1;
+    public static final Integer STOCK_ID = 1;
+    public static final Long TIMESTAMP = LocalDateTime.of(LocalDate.now(), NOON).atZone(systemDefault()).toInstant().toEpochMilli();
+    ;
+
+    public static CreateAccountRequest createMockCreateAccountRequest() {
+
+        return new CreateAccountRequest(CASH);
+    }
+
+    public static Account createMockAccount() {
+
+        return Account
+                .builder()
+                .id(ACCOUNT_ID)
+                .cash(CASH)
+                .issuers(emptyList())
+                .build();
+    }
+
+    public static Stock createMockStock() {
+
+        return Stock.builder()
+                .id(STOCK_ID)
+                .issuerName(ISSUER_NAME)
+                .sharePrice(SHARE_PRICE)
+                .build();
+    }
+
+    public static Issuer createMockIssuer() {
+
+        return Issuer.builder()
+                .id(ISSUER_ID)
+                .account(createMockAccount())
+                .stock(createMockStock())
+                .totalShares(TOTAL_SHARES)
+                .build();
+    }
+
+    public static ProcessedOrder createMockProcessedOrder() {
+
+        return new ProcessedOrder(new CurrentBalance(CASH, singletonList(createMockIssuer())), emptyList());
+    }
+
+    public static SendOrderRequest createMockSendOrderRequest() {
+
+        return new SendOrderRequest(
+                TIMESTAMP,
+                BUY,
+                ISSUER_NAME,
+                TOTAL_SHARES,
+                SHARE_PRICE
+        );
+    }
+}
